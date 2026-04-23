4809878e76b863a12e40fb06d4d690211c47bc5b
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-04-23 14:51:51 +0300

    add some html and js files

---
+++ homework 1/hw1.4/hw1.4.js
@@ -0,0 +1,7 @@
+let name = prompt("Andrian")
+let middleName = prompt("Andriyovich")
+let age = prompt("20")
+
+console.log("Andrian", name);
+console.log("Andriyovich", middleName);
+console.log("20", age)
