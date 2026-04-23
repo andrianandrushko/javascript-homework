11da9bb6b7d4b0993f92e746907796fcff8400fa
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-04-23 14:51:12 +0300

    add some html and js files

---
+++ homework 1/hw1.1/hw1.1.js
@@ -0,0 +1,21 @@
+let a = 'hello'
+console.log(a);
+let b = 'owu'
+console.log(b);
+let c = 'ua'
+console.log(c);
+let d = 'ua'
+console.log(d);
+
+console.log(1);
+console.log(9 + 1);
+console.log(1000 - 1999);
+console.log(120 + 3);
+console.log(355 / 113);
+console.log(1.35 * 2 );
+console.log(15 + 1);
+
+let e = "true"
+console.log(e);
+let f = "false"
+console.log(f);
