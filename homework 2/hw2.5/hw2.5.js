9aa89644c4b0e5b5330e6fa11f6b0c49ea390fcd
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-04-26 14:20:11 +0300

    add some html and js files

---
+++ homework 2/hw2.5/hw2.5.js
@@ -0,0 +1,48 @@
+let day1 = {
+    morning: '+3',
+    daytime: '+11',
+    evening: '+8'
+}
+console.log(day1);
+
+let day2 = {
+    morning: '+5',
+    daytime: '+5',
+    evening: '+7'
+}
+console.log(day2);
+
+let day3 = {
+    morning: '+5',
+    daytime: '+9',
+    evening: '+5'
+}
+console.log(day3);
+
+let day4 = {
+    morning: '+6',
+    daytime: '+9',
+    evening: '+7'
+}
+console.log(day4);
+
+let day5 = {
+    morning: '+5',
+    daytime: '+10',
+    evening: '+7'
+}
+console.log(day5);
+
+let day6 = {
+    morning: '+6',
+    daytime: '+10',
+    evening: '+10'
+}
+console.log(day6);
+
+let day7 = {
+    morning: '+8',
+    daytime: '+12',
+    evening: '+9'
+}
+console.log(day7);
