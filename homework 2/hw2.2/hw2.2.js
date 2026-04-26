e7f55f1aae75539dbf142caed509f3b830247ea0
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-04-26 14:19:34 +0300

    add some html and js files

---
+++ homework 2/hw2.2/hw2.2.js
@@ -0,0 +1,20 @@
+let book1 = {
+    name: 'Garry Potter',
+    pageCount: 320,
+    genre: 'fantasy'
+}
+console.log(book1);
+
+let book2 = {
+    name: 'The Lord of the Rings',
+    pageCount: 704,
+    genre: 'fantasy'
+}
+console.log(book2);
+
+let book3 = {
+    name: 'A Tale of Two Cities',
+    pageCount: 576,
+    genre: 'roman'
+}
+console.log(book3);
