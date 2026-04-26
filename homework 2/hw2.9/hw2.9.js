d13cc4a02f623332ea3e50283c7029ab49766955
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-04-26 14:20:48 +0300

    add some html and js files

---
+++ homework 2/hw2.9/hw2.9.js
@@ -0,0 +1,24 @@
+switch ('6') {
+    case '1':
+        console.log('play football');
+        break;
+    case '2':
+        console.log('go to work');
+        break;
+    case '3':
+        console.log('play games');
+        break;
+    case  '4':
+        console.log('play basketball');
+        break;
+    case  '5':
+        console.log('go to university');
+        break;
+    case  '6':
+        console.log('relax');
+        break;
+    case  '7':
+        console.log('meet with friends');
+        break;
+
+}
