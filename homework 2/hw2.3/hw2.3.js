4d2c8c4e7fe1afea71ce005a996ed34fedb2773e
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-04-26 14:19:43 +0300

    add some html and js files

---
+++ homework 2/hw2.3/hw2.3.js
@@ -0,0 +1,30 @@
+let book1 = {
+    name: 'Garry Potter',
+    pageCount: 320,
+    genre: 'fantasy',
+    authors: 'Joanne Rowling'
+}
+console.log(book1);
+
+let book2 = {
+    name: 'The Lord of the Rings',
+    pageCount: 704,
+    genre: 'fantasy',
+    authors: 'John Ronald Tolkien'
+}
+console.log(book2);
+
+let book3 = {
+    name: 'A Tale of Two Cities',
+    pageCount: 576,
+    genre: 'roman',
+    authors: 'Charles Dickens'
+}
+console.log(book3);
+
+let books = [
+    {id: book1, authors:'Joanne Rowling', age: 60},
+    {id: book2, authors:'John Ronald Tolkien', age: 81},
+    {id: book3, authors:'Charles Dickens', age: 58}
+];
+console.log(books);
