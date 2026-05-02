4d84cc71feb428b21a6b113c3e69b24f3ee4f9c0
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-02 15:03:19 +0300

    add some html and js files

---
+++ Additional homework on arrays and cycles/hw10/hw10.js
@@ -0,0 +1,25 @@
+let arr = [
+    5,
+    'hello',
+    true,
+    10,
+    'world',
+    false,
+    42,
+    'javascript',
+    true,
+    0
+];
+let i = 0;
+if ( i < 10) {
+}
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9])
