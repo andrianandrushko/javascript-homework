d70c097c55768de08639456aaf2554c67e7737e7
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-02 15:03:00 +0300

    add some html and js files

---
+++ Additional homework on arrays and cycles/hw8/hw8.js
@@ -0,0 +1,17 @@
+let arr = [
+    5,
+    'hello',
+    true,
+    10,
+    'world',
+    false,
+    42,
+    'javascript',
+    true,
+    0
+];
+for (let item of arr) {
+    if (typeof item === 'number') {
+        console.log(item);
+    }
+}
