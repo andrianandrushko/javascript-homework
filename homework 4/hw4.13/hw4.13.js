2f6b13ce1a46b47e5d4a7f277cb86a3efdfa2ef2
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-03 17:07:33 +0300

    add some changes

--- homework 4/hw4.13/hw4.13.js
+++ homework 4/hw4.13/hw4.13.js
@@ -1,9 +1,13 @@
-function exchange(sumUAH,currencyValues,exchangeCurrency){
-    let temp = sumUAH[0];
-    sumUAH[currencyValues] = sumUAH[exchangeCurrency]
-    sumUAH[exchangeCurrency] = temp;
+function exchange(sumUAH,currencyValue,exchangeCurrency){
+    let  chooseCurrency;
+    for(let currency of currencyValue){
+        if(currency.currency === exchangeCurrency){
+            chooseCurrency = currency;
+        }
+    }
 
-    return sumUAH;
+    let result = sumUAH / chooseCurrency.value
+    return result;
 }
 
-console.log(exchange(400));
+console.log(exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD'));
