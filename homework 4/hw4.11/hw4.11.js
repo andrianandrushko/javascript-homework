31860da55b81202c133b37dfdebec482f8881818
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-03 13:07:54 +0300

    add some html and js files

---
+++ homework 4/hw4.11/hw4.11.js
@@ -0,0 +1,13 @@
+function sum(arr){
+    let total = arr[0];
+
+    for (sum of arr){
+        if (sum + total){
+            total += sum;
+        }
+    }
+    return total;
+}
+
+
+console.log(sum([1,2,9]));
