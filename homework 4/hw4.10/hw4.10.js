012bcf12a071644fe1835ff598b12175dd592573
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-03 13:07:46 +0300

    add some html and js files

---
+++ homework 4/hw4.10/hw4.10.js
@@ -0,0 +1,13 @@
+function getMin(arr) {
+    let min = arr[0];
+
+    for (let num of arr) {
+        if (num < min) {
+            min = num;
+        }
+    }
+
+    return min;
+}
+
+console.log(getMin([200, 100, 50, 300]));
