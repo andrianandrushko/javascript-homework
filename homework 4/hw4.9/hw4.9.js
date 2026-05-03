3f4ce53bf20a38adf24adbb48ed575b0c3393b0b
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-03 13:07:39 +0300

    add some html and js files

---
+++ homework 4/hw4.9/hw4.9.js
@@ -0,0 +1,11 @@
+function users() {
+    return {id: 1, name: 'alex', age: 30};
+}
+let user = users();
+for (let i = 0; i < 3; i++){
+    document.write(`<ul>
+        <li>${user.id}</li>
+        <li>${user.name}</li>
+        <li>${user.age}</li>
+    </ul>`);
+}
