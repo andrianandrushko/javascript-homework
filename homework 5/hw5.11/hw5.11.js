cf7eb38808332bd59517a571ce6ec13926fe2a75
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-04 18:41:51 +0300

    add some changes

---
+++ homework 5/hw5.11/hw5.11.js
@@ -0,0 +1,12 @@
+const sum = (arr) => {
+    let total = arr[0];
+
+    for ( let sum of arr){
+        if (sum + total){
+            total += sum;
+        }
+    }
+    return total;
+}
+
+console.log(sum([1, 2, 9]));
