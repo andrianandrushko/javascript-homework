5546cba64fb5ec4110607d23be7d87c2a329fcea
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-04 18:41:46 +0300

    add some changes

---
+++ homework 5/hw5.10/hw5.10.js
@@ -0,0 +1,12 @@
+const getMin = (arr) => {
+    let min = arr[0];
+
+    for (let num of arr) {
+        if (num < min){
+            min = num;
+        }
+    }
+    return min;
+}
+
+console.log(getMin([200, 100, 50, 300]));
