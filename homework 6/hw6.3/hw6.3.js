4692b7432cd9edd6467cde83ac1cc8d2ab9b4b4b
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-07 21:59:09 +0300

    add some changes

---
+++ homework 6/hw6.3/hw6.3.js
@@ -0,0 +1,10 @@
+let str = 'HELLO WORLD';
+console.log(str.toLowerCase());
+
+
+let art = 'LOREM IPSUM';
+console.log(art.toLowerCase());
+
+
+let rts = 'JAVASCRIPT IS COOL';
+console.log(rts.toLowerCase());
