14172d925bbac27b8ee741490026ba62cb959a5f
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-12 19:45:31 +0300

    add some changes

---
+++ homework 7/hw7.8/hw7.8.js
@@ -0,0 +1,45 @@
+class Cinderella {
+
+    constructor(id, age, Footsize) {
+        this.id = id;
+        this.age = age;
+        this.Footsize = Footsize;
+    }
+}
+
+class Prince {
+
+    constructor(id, age, slipper) {
+        this.id = id;
+        this.age = age;
+        this.slipper = slipper;
+    }
+}
+
+
+
+let cinderellas = [
+    new Cinderella(1, '22', 38),
+    new Cinderella(2, '23', 37),
+    new Cinderella(3, '24', 36),
+    new Cinderella(4, '21', 35),
+    new Cinderella(5, '26', 40),
+    new Cinderella(6, '28', 41),
+    new Cinderella(7, '25', 42),
+    new Cinderella(8, '20', 34),
+    new Cinderella(9, '22', 35),
+    new Cinderella(10, '22', 38)
+];
+console.log(cinderellas);
+
+for (const cinderella of cinderellas){
+    if (cinderella.Footsize === Prince.slipper){
+        Prince.girl = cinderella;
+    }
+}
+
+let chosenCinderella = cinderellas.find(cinderella =>
+    cinderella.footSize === Prince.slipper
+);
+
+console.log(chosenCinderella);
