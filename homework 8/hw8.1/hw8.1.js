7bbe0775ffd26d53b7cdb099bb602634b2c6a73b
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-05-15 17:08:25 +0300

    add some changes

---
+++ homework 8/hw8.1/hw8.1.js
@@ -0,0 +1,25 @@
+let coursesAndDurationArray = [
+
+    {title: 'JavaScript Complex', monthDuration: 5},
+
+    {title: 'Java Complex', monthDuration: 6},
+
+    {title: 'Python Complex', monthDuration: 6},
+
+    {title: 'QA Complex', monthDuration: 4},
+
+    {title: 'FullStack', monthDuration: 7},
+
+    {title: 'Frontend', monthDuration: 4}
+
+];
+
+
+let result = coursesAndDurationArray.map((value, index) => {
+    return {
+        id: index + 1,
+        title: value.title,
+        monthDuration:value.monthDuration
+    };
+});
+console.log(result);
